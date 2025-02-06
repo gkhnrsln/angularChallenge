ae36402be28243e2441795ac1ef0ef170631d3a8
Gökhan Arslan <goekhan.arslan@outlook.de>
2025-02-06 21:21:51 +0100

    add title for home

--- angular-challenge/src/app/app.component.ts
+++ angular-challenge/src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { FooterComponent } from "./footer/footer.component";
 import { MenuComponent } from "./menu/menu.component";
 import { RouterOutlet } from '@angular/router';
-
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
